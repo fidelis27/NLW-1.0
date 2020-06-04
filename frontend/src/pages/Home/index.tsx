30b012de4efc6121e8c595bd9c5165899330e311
unknown <thiago.fidelis27@gmail.com>
2020-06-04 12:28:11 -0300

    aula 3 escolha da stack app web em REACT | NLW#1 Booster

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+import './styles.css';
+import logo from '../../assets/logo.svg';
+
+const Home: React.FC = () => {
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="Ecoleta" />
+        </header>
+
+        <main>
+          <h1>Seu Marketplace de coleta de resíduos.</h1>
+          <p>Ajudamos pessoas a encotrarem pontos de coleta de resíduos</p>
+          <Link to="/create-point">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </Link>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
