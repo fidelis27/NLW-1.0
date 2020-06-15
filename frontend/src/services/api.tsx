131f5828208dedfd24a808bdb938bccd782f7878
unknown <thiago.fidelis27@gmail.com>
2020-06-15 10:52:12 -0300

    add front config netlify

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL ,
+  baseURL: 'https://server-ecoleta-nodejs.herokuapp.com' ,
 });
 
 export default api;
